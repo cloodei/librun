8e7a67ce3743860663820e8f2b669d9b1641a241
VanHari318 <2351060441@e.tlu.edu.vn>
2025-06-06 10:16:34 +0700

    Add files via upload

---
+++ Admin/Books/UserBooksBorrow.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace librun.Admin.Books
+{
+    partial class UserBooksBorrow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lbl_tb_sach = new System.Windows.Forms.Label();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(128)))), ((int)(((byte)(185)))));
+            this.panel1.Controls.Add(this.lbl_tb_sach);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(427, 66);
+            this.panel1.TabIndex = 1;
+            // 
+            // lbl_tb_sach
+            // 
+            this.lbl_tb_sach.AutoSize = true;
+            this.lbl_tb_sach.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_tb_sach.ForeColor = System.Drawing.Color.White;
+            this.lbl_tb_sach.Location = new System.Drawing.Point(17, 18);
+            this.lbl_tb_sach.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbl_tb_sach.Name = "lbl_tb_sach";
+            this.lbl_tb_sach.Size = new System.Drawing.Size(220, 25);
+            this.lbl_tb_sach.TabIndex = 0;
+            this.lbl_tb_sach.Text = "Danh sách người mượn";
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.AllowUserToDeleteRows = false;
+            this.dataGridView2.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView2.Location = new System.Drawing.Point(0, 66);
+            this.dataGridView2.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.ReadOnly = true;
+            this.dataGridView2.RowHeadersVisible = false;
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(427, 194);
+            this.dataGridView2.TabIndex = 2;
+            // 
+            // UserBooksBorrow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(427, 260);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.panel1);
+            this.Name = "UserBooksBorrow";
+            this.Text = "UserBooksBorrow";
+            this.Load += new System.EventHandler(this.UserBooksBorrow_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lbl_tb_sach;
+        private System.Windows.Forms.DataGridView dataGridView2;
+    }
+}
---
+++ Admin/Books/UserBooksBorrow.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Data.SqlClient;
+
+namespace librun.Admin.Books
+{
+    public partial class UserBooksBorrow : Form
+    {
+        string ketnoi = librun.Properties.Settings.Default.mainConnectionString;
+        SqlConnection connection;
+        DataGridViewCellEventArgs a;
+        SqlDataAdapter da;
+        DataTable dt = new DataTable();
+        long userId;
+        public UserBooksBorrow(long e)
+        {
+            InitializeComponent();
+            this.userId = e;
+        }
+
+        private void UserBooksBorrow_Load(object sender, EventArgs e)
+        {
+            connection = new SqlConnection(ketnoi);
+            connection.Open();
+            string sql = "SELECT  BORROW.user_id, USERS.ten, USERS.email, USERS.trang_thai, BORROW.ngay_muon FROM BORROW INNER JOIN USERS ON BORROW.user_id = USERS.id WHERE book_id = @i";
+            using(var command = new SqlCommand(sql, connection))
+            {
+                command.Parameters.AddWithValue("@i", userId);
+                da = new SqlDataAdapter(command);
+                dt.Clear();
+                da.Fill(dt);
+            }
+            dataGridView2.DataSource = dt;
+            if (dt.Rows.Count > 0)
+            {
+                dataGridView2.Columns[0].HeaderText = "Mã người mượn";
+                dataGridView2.Columns[1].HeaderText = "Họ tên";
+                dataGridView2.Columns[2].HeaderText = "Email";
+                dataGridView2.Columns[3].HeaderText = "Trạng thái";
+                dataGridView2.Columns[4].HeaderText = "Ngày mượn";
+
+            }
+
+        }
+    }
+}
