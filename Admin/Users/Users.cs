64fd022450f98f6affec6bef475f7ee38a90cc04
Duc-Nguyen-05 <leducnguyen07092005@gmail.com>
2025-06-06 14:52:51 +0700

    Update Users.cs

--- Admin/Users/Users.cs
+++ Admin/Users/Users.cs
@@ -12,6 +12,7 @@ namespace Library
         private readonly Color PrimaryColor = Color.FromArgb(0, 123, 255);
         private readonly Color SecondaryColor = Color.FromArgb(40, 44, 52);
 
+
         public Users()
         {
             InitializeComponent();
@@ -74,43 +75,52 @@ namespace Library
                 daQuanLyNguoiDung = new SqlDataAdapter(sql, conn);
                 dtQuanLyNguoiDung = new DataTable();
                 daQuanLyNguoiDung.Fill(dtQuanLyNguoiDung);
-
                 dtQuanLyNguoiDung.Columns.Add("mat_khau_display", typeof(string));
-
-                dgv_quan_ly_nguoi_dung.DataSource = dtQuanLyNguoiDung;
-                dgv_quan_ly_nguoi_dung.Columns[1].HeaderText = "Tên";
-                dgv_quan_ly_nguoi_dung.Columns[2].HeaderText = "Email";
-                dgv_quan_ly_nguoi_dung.Columns[3].Visible = false;
-                dgv_quan_ly_nguoi_dung.Columns[4].HeaderText = "Trạng thái";
-                dgv_quan_ly_nguoi_dung.Columns[5].Visible = false;
-                dgv_quan_ly_nguoi_dung.Columns[6].HeaderText = "Mật khẩu";
-                dgv_quan_ly_nguoi_dung.Columns["mat_khau_display"].DisplayIndex = 3;
-                dgv_quan_ly_nguoi_dung.Columns["mat_khau"].DisplayIndex = 6;
-
                 add_mat_khau_display();
             }
-            catch {
+            catch (Exception ex) {
                 MessageBox.Show("Lỗi khi tải dữ liệu: " , "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
-
         public void add_mat_khau_display()
         {
+            
             foreach (DataRow row in dtQuanLyNguoiDung.Rows)
                 row["mat_khau_display"] = new string('*', row["mat_khau"].ToString().Length);
+
+            dgv_quan_ly_nguoi_dung.DataSource = dtQuanLyNguoiDung;
+
+
+
+            dgv_quan_ly_nguoi_dung.Columns[1].HeaderText = "Tên";
+            dgv_quan_ly_nguoi_dung.Columns[2].HeaderText = "Email";
+            dgv_quan_ly_nguoi_dung.Columns[3].Visible = false;
+            dgv_quan_ly_nguoi_dung.Columns[4].HeaderText = "Trạng thái";
+            dgv_quan_ly_nguoi_dung.Columns[5].Visible = false;
+            dgv_quan_ly_nguoi_dung.Columns[6].HeaderText = "Mật khẩu";
+
+            dgv_quan_ly_nguoi_dung.Columns["mat_khau_display"].DisplayIndex = 3;
+            dgv_quan_ly_nguoi_dung.Columns["mat_khau"].DisplayIndex = 6;
         }
 
         SqlCommand cmd;
         private void btn_them_Click(object sender, EventArgs e)
         {
-            string sql = "INSERT INTO USERS VALUES(N'"+txt_ten.Text+"', N'"+txt_email.Text+"', N'"+cbb_trang_thai.Text+"', N'"+txt_mat_khau.Text+"')";
-            cmd = new SqlCommand(sql, conn);
-            cmd.ExecuteNonQuery();
-            dtQuanLyNguoiDung.Clear();
-            daQuanLyNguoiDung.Fill(dtQuanLyNguoiDung);
-            add_mat_khau_display();
+            var kq = MessageBox.Show("Bạn có chắc chắn muốn thêm?", "Xác nhận thêm", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (kq == DialogResult.Yes)
+            {
+                string sql = "INSERT INTO USERS VALUES(N'" + txt_ten.Text + "', N'" + txt_email.Text + "', N'" + cbb_trang_thai.Text + "', N'" + txt_mat_khau.Text + "')";
+                cmd = new SqlCommand(sql, conn);
+                cmd.ExecuteNonQuery();
+                dtQuanLyNguoiDung.Clear();
+                daQuanLyNguoiDung.Fill(dtQuanLyNguoiDung);
+                add_mat_khau_display();
+                txt_tim_kiem_TextChanged(sender, e);
+                MessageBox.Show("Bạn đã thêm thành công", "Thông báo");
+            }
         }
 
+        string email_current;
         DataGridViewRow row_befor = null;
         private void dgv_quan_ly_nguoi_dung_CellClick(object sender, DataGridViewCellEventArgs e)
         {
@@ -118,50 +128,60 @@ namespace Library
             {
                 row_befor.DefaultCellStyle.BackColor = Color.White;
             }
-            dgv_quan_ly_nguoi_dung.CurrentRow.DefaultCellStyle.BackColor = Color.LightBlue;
             txt_ten.Text = dgv_quan_ly_nguoi_dung.CurrentRow.Cells[1].Value.ToString();
             txt_email.Text = dgv_quan_ly_nguoi_dung.CurrentRow.Cells[2].Value.ToString();
             txt_mat_khau.Text = dgv_quan_ly_nguoi_dung.CurrentRow.Cells[3].Value.ToString();
             cbb_trang_thai.Text = dgv_quan_ly_nguoi_dung.CurrentRow.Cells[4].Value.ToString();
 
+            dgv_quan_ly_nguoi_dung.CurrentRow.DefaultCellStyle.BackColor = Color.LightBlue;
             row_befor = dgv_quan_ly_nguoi_dung.CurrentRow;
+            
+
         }
 
         private void btn_sua_Click(object sender, EventArgs e)
         {
-            string idUser = dgv_quan_ly_nguoi_dung.CurrentRow.Cells["id"].Value.ToString();
-            cmd = new SqlCommand(
-                "UPDATE USERS SET " +
-                "ten = N'"+txt_ten.Text+"', email = N'"+txt_email.Text+"', trang_thai = N'"+cbb_trang_thai.Text+"', mat_khau = N'"+txt_mat_khau.Text+"' " +
-                "WHERE id = "+idUser+"",
-                conn
-            );
-            cmd.ExecuteNonQuery();
-
-            dtQuanLyNguoiDung.Clear();
-            daQuanLyNguoiDung.Fill(dtQuanLyNguoiDung);
-            add_mat_khau_display();
+            
+            var kq = MessageBox.Show("Bạn có chắc chắn muốn sửa?", "Xác nhận sửa", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (kq == DialogResult.Yes)
+            {
+                string idUser = dgv_quan_ly_nguoi_dung.CurrentRow.Cells["id"].Value.ToString();
+                cmd = new SqlCommand(
+                    "UPDATE USERS SET " +
+                    "ten = N'" + txt_ten.Text + "', " +
+                     "email = N'" + txt_email.Text + "', " +
+                     "trang_thai = N'" + cbb_trang_thai.Text + "', " +
+                     "mat_khau = N'" + txt_mat_khau.Text + "' " +
+                     "WHERE id = " + idUser,
+                     conn
+                 );
+                cmd.ExecuteNonQuery();
+                
+                //dtQuanLyNguoiDung.Clear();
+                //daQuanLyNguoiDung.Fill(dtQuanLyNguoiDung);
+                txt_tim_kiem_TextChanged(sender, e);
+                MessageBox.Show("Bạn đã sửa thành công", "Thông báo");
+            }
         }
 
         SqlDataAdapter daTimKiem;
         private void txt_tim_kiem_TextChanged(object sender, EventArgs e)
         {
-            string sql = "select ROW_NUMBER() OVER(ORDER BY id) as STT, ten, email, mat_khau, trang_thai " +
+            string sql = "select ROW_NUMBER() OVER(ORDER BY id) as STT, ten, email, mat_khau, trang_thai, id " +
                 "from USERS " +
                 "where (ten like N'%"+txt_tim_kiem.Text+"%' or email like N'%"+txt_tim_kiem.Text+"%')";
             if (cbb_loc.Text == "Hoạt động")
             {
-                sql += " and trang_thai = N'Hoạt động'";
-            }
-            else if(cbb_loc.Text == "Khóa")
+                sql = sql + " and trang_thai = N'Hoạt động'";
+            }else if(cbb_loc.Text == "Khóa")
             {
-                sql += " and trang_thai = N'Khóa'";
+                sql = sql+" and trang_thai = N'Khóa'";
             }
 
-            daTimKiem = new SqlDataAdapter(sql, conn);
+                daTimKiem = new SqlDataAdapter(sql, conn);
             dtQuanLyNguoiDung.Clear();
             daTimKiem.Fill(dtQuanLyNguoiDung);
-            add_mat_khau_display();
+            add_mat_khau_display() ;
         }
 
         SqlDataAdapter daTBSach;
@@ -184,20 +204,26 @@ namespace Library
 
         private void btn_xoa_Click(object sender, EventArgs e)
         {
-            if (txt_email.Text!="" && MessageBox.Show("Bạn có chắc chắn muốn xóa?", "Xác nhận xóa", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            if (txt_email.Text!="" )
             {
-                var idUser = dgv_quan_ly_nguoi_dung.CurrentRow.Cells[5].Value.ToString();
-                string sql = "delete from USERS where id = " + idUser + "";
-                cmd = new SqlCommand(sql, conn);
-                cmd.ExecuteNonQuery();
-                dtQuanLyNguoiDung.Clear();
-                daQuanLyNguoiDung.Fill(dtQuanLyNguoiDung);
-                add_mat_khau_display();
-                txt_ten.Text = "";
-                txt_email.Text = "";
-                txt_mat_khau.Text = "";
-                cbb_trang_thai.Text = "";
-                MessageBox.Show("Bạn đã xóa thành công","Thông báo");
+                var kq = MessageBox.Show("Bạn có chắc chắn muốn xóa?", "Xác nhận xóa", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (kq == DialogResult.Yes)
+                {
+                    var idUser = dgv_quan_ly_nguoi_dung.CurrentRow.Cells[5].Value.ToString();
+                    string sql = "delete from USERS where id = " + idUser + "";
+                    cmd = new SqlCommand(sql, conn);
+                    cmd.ExecuteNonQuery();
+                    dtQuanLyNguoiDung.Clear();
+                    daQuanLyNguoiDung.Fill(dtQuanLyNguoiDung);
+                    add_mat_khau_display();
+                    txt_ten.Text = "";
+                    txt_email.Text = "";
+                    txt_mat_khau.Text = "";
+                    cbb_trang_thai.Text = "";
+                    txt_tim_kiem_TextChanged(sender, e);
+                    MessageBox.Show("Bạn đã xóa thành công", "Thông báo");
+                }
+                
             }
             else
             {
