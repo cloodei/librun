d54243795c024289196c2abbd114415bccd467c0
VanHari318 <2351060441@e.tlu.edu.vn>
2025-06-08 11:50:24 +0700

    Update UserBooksBorrow.cs

--- Admin/Books/UserBooksBorrow.cs
+++ Admin/Books/UserBooksBorrow.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -48,7 +48,54 @@ namespace Library
                 dataGridView2.Columns[4].HeaderText = "Ngày mượn";
 
             }
+            for (int i = 0; i < dataGridView2.RowCount; i++)
+            {
+                if (dataGridView2.Rows[i].Cells[3].Value.ToString() == "Hoạt động")
+                {
+                    dataGridView2.Rows[i].DefaultCellStyle.BackColor = Color.White;
+                }
+                else
+                {
+                    dataGridView2.Rows[i].DefaultCellStyle.BackColor = Color.LightPink;
+                }
+            }
+        }
 
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            string keyword = ((TextBox)sender).Text.Trim();
+
+            string sql = "SELECT  BORROW.user_id, USERS.ten, USERS.email, USERS.trang_thai, BORROW.ngay_muon FROM BORROW INNER JOIN USERS ON BORROW.user_id = USERS.id WHERE book_id = @i AND ten LIKE @ten";
+            using (SqlCommand command = new SqlCommand(sql, connection))
+            {
+                command.Parameters.AddWithValue("@ten", "%" + keyword + "%");
+                command.Parameters.AddWithValue("@i", userId);
+                da = new SqlDataAdapter(command);
+                dt.Clear();
+                da.Fill(dt);
+            }
+
+            dataGridView2.DataSource = dt;
+            if (dt.Rows.Count > 0)
+            {
+                dataGridView2.Columns[0].HeaderText = "Mã người mượn";
+                dataGridView2.Columns[1].HeaderText = "Họ tên";
+                dataGridView2.Columns[2].HeaderText = "Email";
+                dataGridView2.Columns[3].HeaderText = "Trạng thái";
+                dataGridView2.Columns[4].HeaderText = "Ngày mượn";
+
+            }
+            for (int i = 0; i < dataGridView2.RowCount; i++)
+            {
+                if (dataGridView2.Rows[i].Cells[3].Value.ToString() == "Hoạt động")
+                {
+                    dataGridView2.Rows[i].DefaultCellStyle.BackColor = Color.White;
+                }
+                else
+                {
+                    dataGridView2.Rows[i].DefaultCellStyle.BackColor = Color.LightPink;
+                }
+            }
         }
     }
 }
