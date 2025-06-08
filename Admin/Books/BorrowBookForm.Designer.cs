64496e11f97f4bba8615b368e3cbaff33565a7fe
VanHari318 <2351060441@e.tlu.edu.vn>
2025-06-08 11:51:09 +0700

    Add files via upload

---
+++ Admin/Books/BorrowBookForm.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace librun.Admin.Books
+{
+    partial class BorrowBookForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(159, 139);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(356, 20);
+            this.dateTimePicker1.TabIndex = 14;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox4.Location = new System.Drawing.Point(159, 98);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(356, 20);
+            this.textBox4.TabIndex = 13;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.Location = new System.Drawing.Point(159, 54);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(356, 20);
+            this.textBox2.TabIndex = 11;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(159, 16);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(356, 20);
+            this.textBox1.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(25, 139);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(110, 20);
+            this.label5.TabIndex = 19;
+            this.label5.Text = "Ngày xuất bản";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(71, 96);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 20);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "Thể loại";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(63, 184);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 20);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Nội dung";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(75, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 20);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Tác giả";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(74, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 20);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Tiêu đề";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Location = new System.Drawing.Point(159, 184);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(356, 59);
+            this.richTextBox1.TabIndex = 20;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(128)))), ((int)(((byte)(185)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(159, 260);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 41);
+            this.button1.TabIndex = 21;
+            this.button1.Text = "Thêm";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(128)))), ((int)(((byte)(185)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(289, 260);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(84, 41);
+            this.button2.TabIndex = 23;
+            this.button2.Text = "Xóa";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(128)))), ((int)(((byte)(185)))));
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(159, 260);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(84, 41);
+            this.button3.TabIndex = 24;
+            this.button3.Text = "Sửa";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // BorrowBookForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Cornsilk;
+            this.ClientSize = new System.Drawing.Size(547, 302);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.MinimumSize = new System.Drawing.Size(563, 341);
+            this.Name = "BorrowBookForm";
+            this.Text = "BorrowBookForm";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.BorrowBookForm_FormClosed);
+            this.Load += new System.EventHandler(this.BorrowBookForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.DateTimePicker dateTimePicker1;
+        public System.Windows.Forms.TextBox textBox4;
+        public System.Windows.Forms.TextBox textBox2;
+        public System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        public System.Windows.Forms.RichTextBox richTextBox1;
+        public System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        public System.Windows.Forms.Button button3;
+    }
+}
---
+++ Admin/Books/BorrowBookForm.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace librun.Admin.Books
+{
+    public partial class BorrowBookForm : Form
+    {
+        string ketnoi = librun.Properties.Settings.Default.mainConnectionString;
+        SqlConnection connection;
+        SqlDataAdapter da;
+        DataTable dt = new DataTable();
+        public long ide;
+        public Form form;
+        public BorrowBookForm(Form bf, SqlDataAdapter da, DataTable dt, long r)
+        {
+            InitializeComponent();
+            this.form = bf;
+            this.da = da;
+            this.dt = dt;
+            this.ide = r;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length == 0)
+            {
+                MessageBox.Show("Nhập lại tên sách");
+                return;
+            }
+
+            if (textBox2.Text.Length == 0)
+            {
+                MessageBox.Show("Nhập lại tên tác giả");
+                return;
+            }
+
+            if (richTextBox1.Text.Length == 0)
+            {
+                MessageBox.Show("Nhập lại nội dung sách");
+                return;
+            }
+
+            if (textBox4.Text.Length == 0)
+            {
+                MessageBox.Show("Nhập lại thể loại sách");
+                return;
+            }
+            if (ktra())
+            {
+                MessageBox.Show("Sách đã tồn tại");
+                return;
+            }
+            DialogResult result = MessageBox.Show(
+                "Thêm sách không?",
+                "chắc chưa?",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question
+            );
+            if (result == DialogResult.No)
+            {
+                return;
+            }
+
+            string sql = "INSERT INTO BOOKS (tieu_de, ten_tac_gia, noi_dung, the_loai, ngay_xuat_ban) VALUES (@ten, @tac, @nd, @tl, @nxb)";
+            using (var com = new SqlCommand(sql, connection))
+            {
+                com.Parameters.AddWithValue("@ten", textBox1.Text);
+                com.Parameters.AddWithValue("@tac", textBox2.Text);
+                com.Parameters.AddWithValue("@nd", richTextBox1.Text);
+                com.Parameters.AddWithValue("@tl", textBox4.Text);
+                com.Parameters.AddWithValue("@nxb", dateTimePicker1.Value);
+                com.ExecuteNonQuery();
+            }
+            dt.Clear();
+            da.Fill(dt);
+            textBox1.Text = "";
+            textBox2.Text = "";
+            richTextBox1.Text = "";
+            textBox4.Text = "";
+            dateTimePicker1.Value = DateTime.Now;
+        }
+
+        public bool ktra()
+        {
+            string sql = "SELECT COUNT(*) FROM Books WHERE tieu_de = @ten";
+            using (var com = new SqlCommand(sql, connection))
+            {
+                com.Parameters.AddWithValue("@ten", textBox1.Text);
+                return (int)com.ExecuteScalar() > 0;
+            }
+        }
+        private void BorrowBookForm_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'libDataSet2.BOOKS' table. You can move, or remove it, as needed.
+            connection = new SqlConnection(ketnoi);
+            connection.Open();
+
+        }
+
+        private void BorrowBookForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            form.Enabled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            richTextBox1.Text = "";
+            textBox4.Text = "";
+            dateTimePicker1.Value = DateTime.Now;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length == 0)
+            {
+                MessageBox.Show("Nhập lại tên sách");
+                return;
+            }
+
+            if (textBox2.Text.Length == 0)
+            {
+                MessageBox.Show("Nhập lại tên tác giả");
+                return;
+            }
+
+            if (richTextBox1.Text.Length == 0)
+            {
+                MessageBox.Show("Nhập lại nội dung sách");
+                return;
+            }
+
+            if (textBox4.Text.Length == 0)
+            {
+                MessageBox.Show("Nhập lại thể loại sách");
+                return;
+            }
+            if (ktra())
+            {
+                MessageBox.Show("Sách đã tồn tại");
+                return;
+            }
+            DialogResult result = MessageBox.Show(
+                "Sửa sách không?",
+                "chắc chưa?",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question
+            );
+            if (result == DialogResult.No)
+            {
+                return;
+            }
+            string sql = "UPDATE BOOKS SET tieu_de = @ten, ten_tac_gia = @tac, noi_dung = @nd, the_loai = @tl, ngay_xuat_ban = @nxb WHERE id = @id";
+            using (SqlCommand com = new SqlCommand(sql, connection))
+            {
+                com.Parameters.AddWithValue("@id", ide);
+                com.Parameters.AddWithValue("@ten", textBox1.Text);
+                com.Parameters.AddWithValue("@tac", textBox2.Text);
+                com.Parameters.AddWithValue("@nd", richTextBox1.Text);
+                com.Parameters.AddWithValue("@tl", textBox4.Text);
+                com.Parameters.AddWithValue("@nxb", dateTimePicker1.Value);
+                com.ExecuteNonQuery();
+                MessageBox.Show("Cập nhật thành công");
+            }
+            dt.Clear();
+            da.Fill(dt);
+            textBox1.Text = "";
+            textBox2.Text = "";
+            richTextBox1.Text = "";
+            textBox4.Text = "";
+            dateTimePicker1.Value = DateTime.Now;
+            form.Enabled = true;
+            this.Close();
+        }
+    }
+}
