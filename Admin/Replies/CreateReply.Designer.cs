8f0b428d90d3e075c72556c5c8458018fcd7a860
Duc-Nguyen-05 <leducnguyen07092005@gmail.com>
2025-06-09 20:22:01 +0700

    Update CreateReply.Designer.cs

--- Admin/Replies/CreateReply.Designer.cs
+++ Admin/Replies/CreateReply.Designer.cs
@@ -1,126 +1,115 @@
-﻿namespace librun.Admin.Replies
+namespace librun.Admin.Replies
 {
     partial class CreateReply
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lbl_noi_dung;
+        private System.Windows.Forms.Label lbl_nhap_hoi_dap;
+        private System.Windows.Forms.RichTextBox rtxtb_nhap_hoi_dap;
+        private System.Windows.Forms.Button btn_gui_hoi_dap;
+
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
-            {
                 components.Dispose();
-            }
             base.Dispose(disposing);
         }
 
         #region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
+            this.lblTitle = new System.Windows.Forms.Label();
             this.lbl_noi_dung = new System.Windows.Forms.Label();
-            this.rtxtb_nhap_hoi_dap = new System.Windows.Forms.RichTextBox();
             this.lbl_nhap_hoi_dap = new System.Windows.Forms.Label();
+            this.rtxtb_nhap_hoi_dap = new System.Windows.Forms.RichTextBox();
             this.btn_gui_hoi_dap = new System.Windows.Forms.Button();
-            this.panel1.SuspendLayout();
-            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
-            // panel1
+            // lblTitle
             // 
-            this.panel1.Controls.Add(this.lbl_noi_dung);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(614, 160);
-            this.panel1.TabIndex = 0;
-            // 
-            // panel2
-            // 
-            this.panel2.Controls.Add(this.btn_gui_hoi_dap);
-            this.panel2.Controls.Add(this.lbl_nhap_hoi_dap);
-            this.panel2.Controls.Add(this.rtxtb_nhap_hoi_dap);
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel2.Location = new System.Drawing.Point(0, 160);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(614, 229);
-            this.panel2.TabIndex = 1;
+            this.lblTitle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold);
+            this.lblTitle.ForeColor = System.Drawing.Color.White;
+            this.lblTitle.Location = new System.Drawing.Point(0, 0);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
+            this.lblTitle.Size = new System.Drawing.Size(500, 50);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Phản hồi người dùng";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // lbl_noi_dung
             // 
-            this.lbl_noi_dung.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.lbl_noi_dung.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_noi_dung.Location = new System.Drawing.Point(0, 0);
+            this.lbl_noi_dung.BackColor = System.Drawing.Color.White;
+            this.lbl_noi_dung.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl_noi_dung.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.lbl_noi_dung.Location = new System.Drawing.Point(20, 60);
             this.lbl_noi_dung.Name = "lbl_noi_dung";
-            this.lbl_noi_dung.Size = new System.Drawing.Size(614, 160);
-            this.lbl_noi_dung.TabIndex = 0;
-            this.lbl_noi_dung.Text = "Nội dung: ";
-            // 
-            // rtxtb_nhap_hoi_dap
-            // 
-            this.rtxtb_nhap_hoi_dap.Location = new System.Drawing.Point(147, 26);
-            this.rtxtb_nhap_hoi_dap.Name = "rtxtb_nhap_hoi_dap";
-            this.rtxtb_nhap_hoi_dap.Size = new System.Drawing.Size(405, 96);
-            this.rtxtb_nhap_hoi_dap.TabIndex = 0;
-            this.rtxtb_nhap_hoi_dap.Text = "";
+            this.lbl_noi_dung.Padding = new System.Windows.Forms.Padding(10);
+            this.lbl_noi_dung.Size = new System.Drawing.Size(440, 80);
+            this.lbl_noi_dung.TabIndex = 1;
+            this.lbl_noi_dung.Text = "Nội dung: ...";
             // 
             // lbl_nhap_hoi_dap
             // 
             this.lbl_nhap_hoi_dap.AutoSize = true;
-            this.lbl_nhap_hoi_dap.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_nhap_hoi_dap.Location = new System.Drawing.Point(12, 64);
+            this.lbl_nhap_hoi_dap.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.lbl_nhap_hoi_dap.Location = new System.Drawing.Point(20, 160);
             this.lbl_nhap_hoi_dap.Name = "lbl_nhap_hoi_dap";
-            this.lbl_nhap_hoi_dap.Size = new System.Drawing.Size(112, 20);
-            this.lbl_nhap_hoi_dap.TabIndex = 1;
+            this.lbl_nhap_hoi_dap.Size = new System.Drawing.Size(119, 23);
+            this.lbl_nhap_hoi_dap.TabIndex = 2;
             this.lbl_nhap_hoi_dap.Text = "Nhập hồi đáp:";
+            // 
+            // rtxtb_nhap_hoi_dap
+            // 
+            this.rtxtb_nhap_hoi_dap.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.rtxtb_nhap_hoi_dap.Location = new System.Drawing.Point(20, 190);
+            this.rtxtb_nhap_hoi_dap.Name = "rtxtb_nhap_hoi_dap";
+            this.rtxtb_nhap_hoi_dap.Size = new System.Drawing.Size(440, 100);
+            this.rtxtb_nhap_hoi_dap.TabIndex = 3;
+            this.rtxtb_nhap_hoi_dap.Text = "";
             // 
             // btn_gui_hoi_dap
             // 
-            this.btn_gui_hoi_dap.Location = new System.Drawing.Point(205, 157);
+            this.btn_gui_hoi_dap.BackColor = System.Drawing.Color.White;
+            this.btn_gui_hoi_dap.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.btn_gui_hoi_dap.FlatAppearance.BorderSize = 2;
+            this.btn_gui_hoi_dap.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_gui_hoi_dap.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.btn_gui_hoi_dap.Location = new System.Drawing.Point(180, 310);
             this.btn_gui_hoi_dap.Name = "btn_gui_hoi_dap";
-            this.btn_gui_hoi_dap.Size = new System.Drawing.Size(219, 43);
-            this.btn_gui_hoi_dap.TabIndex = 2;
+            this.btn_gui_hoi_dap.Size = new System.Drawing.Size(120, 35);
+            this.btn_gui_hoi_dap.TabIndex = 4;
             this.btn_gui_hoi_dap.Text = "Gửi";
-            this.btn_gui_hoi_dap.UseVisualStyleBackColor = true;
+            this.btn_gui_hoi_dap.UseVisualStyleBackColor = false;
             this.btn_gui_hoi_dap.Click += new System.EventHandler(this.btn_gui_hoi_dap_Click);
             // 
             // CreateReply
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(614, 389);
-            this.Controls.Add(this.panel2);
-            this.Controls.Add(this.panel1);
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(500, 380);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.lbl_noi_dung);
+            this.Controls.Add(this.lbl_nhap_hoi_dap);
+            this.Controls.Add(this.rtxtb_nhap_hoi_dap);
+            this.Controls.Add(this.btn_gui_hoi_dap);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "CreateReply";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-            this.Text = "CreateReply";
+            this.Text = "Tạo phản hồi";
             this.Load += new System.EventHandler(this.CreateReply_Load);
-            this.panel1.ResumeLayout(false);
-            this.panel2.ResumeLayout(false);
-            this.panel2.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.Label lbl_noi_dung;
-        private System.Windows.Forms.RichTextBox rtxtb_nhap_hoi_dap;
-        private System.Windows.Forms.Button btn_gui_hoi_dap;
-        private System.Windows.Forms.Label lbl_nhap_hoi_dap;
     }
 }
